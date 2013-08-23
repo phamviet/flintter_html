e3c9005c846e344769c3147d14417a5640f8f9a9
Viet <phpcodervn@gmail.com>
2013-08-23 14:59:06 +0700

    - remove old assets - add Homepage.js

---
+++ assets/js/apps/main/application/Homepage.js
@@ -0,0 +1,15 @@
+define([
+    '$',
+    'App'
+], function($, undefinedObject, App) {
+
+    var Homepage = function() {
+
+    };
+
+    Homepage.prototype.initialize = function() {
+
+    };
+
+    return Homepage;
+});
