2c49a3a998790ffcb6ae215a5b92fff4217f039d
Viet <phpcodervn@gmail.com>
2013-10-15 01:58:21 +0700

    - update

--- assets/js/app/module/user/relationship.js
+++ assets/js/app/module/user/relationship.js
@@ -6,13 +6,21 @@ define(['$'], function($) {
     var handlers =  {
         requestFriend: function($el) {
             var friendId = $el.data('user-id');
-
             $.post(SITE.url('service/relationship/friend/request'), {id: friendId}, function(res){
                 if(res.status) {
-                    $el.hide();
-                    $('#people-may-know-' + friendId).slideUp(1000, function(){
-                        $(this).remove();
-                    });
+                    $el.off();
+                    if ($el.parent().prop("tagName") == 'LI') {
+                        $el.parent().remove();
+                    } else {
+                        $el.remove();
+                    }
+
+                    var me = $('#people-may-know-' + friendId);
+                    if(me.length) {
+                        me.slideUp(1000, function() {
+                            $(this).remove();
+                        });
+                    }
                 }
             }, 'json');
         }
